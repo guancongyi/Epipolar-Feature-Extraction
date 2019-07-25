47440aaa7aac6e7c415ba00af22ae121ce4c6791
guancongyi <congyiguan@gmail.com>
2019-07-25 11:16:43 +0800

    finished epipolar matching by given some preset values y value differences(30), get an error range.

--- epipolar.py
+++ epipolar.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 
 class EpipolarMatching:
@@ -10,6 +11,10 @@ class EpipolarMatching:
         self.des1 = []
         self.pts2 = []
         self.des2 = []
+        self.matches = []
+        self.goodMatches = []
+        self.error = 30
+
 
     def drawPts(self):
         img1 = self.im1
@@ -30,6 +35,15 @@ class EpipolarMatching:
         cv2.imshow('2', img2)
         cv2.waitKey(0)
 
+    def drawMatches(self, matches):
+        # matches = sorted(self.matches, key=lambda x: x.distance)
+        draw_params = dict(matchColor=(0, 255, 0),singlePointColor = (255, 0, 0),flags=0)
+        img3 = cv2.drawMatches(self.im1, self.pts1, self.im2, self.pts2, matches, None, flags=0)
+        # img3 = cv2.resize(img3, (1400, 1300), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite('3.jpg', img3)
+
+
+
     def extract(self):
         # Initiate FAST detector
         orb1 = cv2.ORB_create()
@@ -38,24 +52,66 @@ class EpipolarMatching:
         pts2 = orb2.detect(self.im2, None)
         self.pts1, self.des1 = orb1.compute(self.im1, pts1)
         self.pts2, self.des2 = orb2.compute(self.im2, pts2)
-        #self.drawPts()
+
+    def BFmatch(self):
+        # FLANN_INDEX_KDTREE = 0
+        # index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        # search_params = dict(checks=50)  # or pass empty dictionary
+        # flann = cv2.FlannBasedMatcher(index_params, search_params)
+        # matches = flann.knnMatch(self.des1, self.des2, k=2)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        self.matches = bf.match(self.des1, self.des2)
+
+    def EpipolarMatch(self):
+        # Every keypoint in image 1,its correspondent in image 2
+        # should be on the same line as the one in image 1.
+        count = 0
+        sum = 0
+        for match in self.matches:
+            x1, y1 = self.pts1[match.queryIdx].pt
+            x2, y2 = self.pts2[match.trainIdx].pt
+            error = abs(y2 - y1)
+            if error <= self.error:
+                count += 1
+                sum += error
+                self.goodMatches.append(match)
 
 
-    def match(self):
+        self.printMatches(self.goodMatches,self.pts1,self.pts2)
+        self.drawMatches(self.goodMatches)
+        print("error " , sum/count)
 
-        pass
+    def printMatches(self, matches, pts1, pts2):
+        for match in matches:
+            print(pts1[match.queryIdx].pt, pts2[match.trainIdx].pt)
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
     im1 = 'result0.jpg'  # 7230*6742
-    im2 = 'result3.jpg' # 7222*6738
+    im2 = 'result4.jpg' # 7222*6738
+    # im1 = 'test/E00224_40814-thred.jpg'
+    # im2 = 'test/E00223_40813-thred.jpg'
     m = EpipolarMatching(im1, im2)
+    start = time.time()
     m.extract()
+    end1 = time.time()
+    print(end1 - start)
 
+    m.BFmatch()
+    end2 = time.time()
+    print(end2 - end1)
 
-    # cv2.imwrite('match.jpg', img3)
+    m.EpipolarMatch()
+    end3 = time.time()
+    print(end3 - end2)
 
+    print("done")
 
 
 
